b6c3594ed865dbb97c115683ac96d1c271175c95
Jack <70027123+jackwu0528@users.noreply.github.com>
2023-04-17 02:53:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@
 namespace tools {
     //% weight=0
     //% blockId="turn_on_fill_light" block="Trun On Fill Light"
-    export function TrunOnFillLight(): void {
+    export function turn_on_fill_light(): void {
         
     }
 
     //% weight=0
     //% blockId="turn_off_fill_light" block="Trun Off Fill Light"
-    export function TrunOffFillLight(): void {
+    export function turn_off_fill_light(): void {
        
     }
 }
