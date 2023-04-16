2819a3405bb4b04b64340ef9ad56a3809b2aedfe
Jack <70027123+jackwu0528@users.noreply.github.com>
2023-04-17 02:33:51 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# tools
+
+這是專為測試用的micro:bit積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+tools=github:lioujj/pxt-tools
+```
---
+++ main.ts
@@ -0,0 +1,19 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
+namespace tools {
+    /**
+    * 計算長方形面積，並回傳
+    */
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function areaOfRectangle(length: number, width:number): number {
+        return length*width
+    }
+    /**
+    * 計算長方形面積，不回傳，只顯示在LED
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(length*width)
+    }
+}
