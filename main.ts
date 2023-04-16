9ce146810b5fc6f62a99ce78b47e56d5889503a9
Jack <70027123+jackwu0528@users.noreply.github.com>
2023-04-17 02:42:42 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,19 +1,14 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    /**
-    * 計算長方形面積，並回傳
-    */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
+    //% weight=0
+    //% blockId="turn_on_fill_light" block="Trun On Fill Light"
+    export function TrunOnFillLight(): void {
+        
     }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
+
+    //% weight=0
+    //% blockId="turn_off_fill_light" block="Trun Off Fill Light"
+    export function TrunOffFillLight(): void {
+       
     }
 }
