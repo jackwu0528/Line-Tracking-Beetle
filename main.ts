208669159d5099373b978b228b64f4f316ada3f6
Jack <70027123+jackwu0528@users.noreply.github.com>
2023-04-17 15:19:03 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -3,12 +3,22 @@ namespace LineTrackingBeetle {
     //% weight=0
     //% blockId="turn_on_fill_light" block="Trun On Fill Light"
     export function turn_on_fill_light(): void {
-        
+        let buf: Buffer = pins.createBuffer(2);
+
+        buf[0] = 0x03;
+        buf[1] = 0x00;
+
+        pins.i2cWriteBuffer(0x12, buf, false);
     }
 
     //% weight=0
     //% blockId="turn_off_fill_light" block="Trun Off Fill Light"
     export function turn_off_fill_light(): void {
-       
+        let buf: Buffer = pins.createBuffer(2);
+
+        buf[0] = 0x03;
+        buf[1] = 0x01;
+
+        pins.i2cWriteBuffer(0x12, buf, false);
     }
 }
