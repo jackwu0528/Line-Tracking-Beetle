42cf8788fb48db1ad7d33ac4aad9247dd0903e48
Jack <70027123+jackwu0528@users.noreply.github.com>
2023-04-27 00:00:25 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-let WB_RGB = [0.52,1,1]
+let WB_RGB = [0.52, 1, 1]
 let LINE_THRESHOLD = 150
 
-enum Servos {
-    //% blockId="S1" block="Uplift (S1)"
-    uplift_servo = 0,
-    //% blockId="S2" block="Plier (S2)"
-    plier_servo = 1
+const enum Servos {
+    //% blockId="S1" block="Lift (S1)"
+    lift_servo = 0,
+    //% blockId="S2" block="Gripper (S2)"
+    gripper_servo = 1
 }
 
-enum Color {
+const enum Color {
     //%block="Red"
     RED = 1,
     //%block="Green"
@@ -21,8 +21,8 @@ enum Color {
     WHITH = 5
 }
 
-enum Patrol {
-    //% block="Q1"
+const enum Patrol {
+    //%block="Q1"
     Q1 = 33,
     //%block="Q2"
     Q2 = 34,
@@ -102,15 +102,15 @@ namespace MaqueenMechanicBeetle {
         pins.i2cWriteNumber(0x39, 0x96, NumberFormat.UInt8LE)
         let buf = pins.i2cReadBuffer(0x39, 6)
 
-        let color_r = buf[1]*256 + buf[0]
-        let color_g = buf[3]*256 + buf[2]
-        let color_b = buf[5]*256 + buf[4]
-        color_r = Math.floor(color_r/WB_RGB[0])
-        color_g = Math.floor(color_g/WB_RGB[1])
-        color_b = Math.floor(color_b/WB_RGB[2])
+        let color_r = buf[1] * 256 + buf[0]
+        let color_g = buf[3] * 256 + buf[2]
+        let color_b = buf[5] * 256 + buf[4]
+        color_r = Math.floor(color_r / WB_RGB[0])
+        color_g = Math.floor(color_g / WB_RGB[1])
+        color_b = Math.floor(color_b / WB_RGB[2])
 
         let maxColor = Math.max(color_r, Math.max(color_g, color_b))
-        if(maxColor > 255) {        
+        if (maxColor > 255) {
             let scale = 255 / maxColor
             color_r = Math.floor(color_r * scale)
             color_g = Math.floor(color_g * scale)
@@ -122,16 +122,22 @@ namespace MaqueenMechanicBeetle {
 
 
     //% weight=0
-    //% block="read line-tracking sensor|%patrol grayscale "
+    //% block="Read Line-Tracking Sensor|%patrol Grayscale"
     export function readPatrolVoltage(patrol: Patrol): number {
         pins.i2cWriteNumber(0x12, patrol, NumberFormat.UInt8LE)
         return pins.i2cReadNumber(0x12, NumberFormat.UInt8LE);
     }
-    
+
 
     //% weight=0
-    //%block="read line-tracking sensor|%patrol"
-    export function readPatrol(patrol: Patrol): number {
-        return readPatrolVoltage(patrol) > LINE_THRESHOLD ? 1 : 0;
+    //%block="Read Line-Tracking Sensor|%patrol"
+    export function readPatrol(patrol: Patrol): boolean {
+        return readPatrolVoltage(patrol) > LINE_THRESHOLD;
+    }
+
+
+    //% weight=0
+    //%block="Line-Tracking"
+    export function line_tracking(): void {
     }
 }
